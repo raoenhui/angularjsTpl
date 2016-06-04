b64fdcc4b6641eb5ed07e8a7d22fe0f856c94ef4
ice <ice@icedeMacBook-Pro.local>
2016-06-05 01:38:52 +0800

    first commit

---
+++ AngularJS2-8/02-05/app/HelloAngular_Directive.js
@@ -0,0 +1,9 @@
+var myModule = angular.module("MyModule", []);
+myModule.directive("hello", function() {
+    return {
+        // restrict: 'AEMC',
+        restrict: 'AEMC',
+        template: '<div>Hi everyone!</div>',
+        replace: true
+    }
+});
